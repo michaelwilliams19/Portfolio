7f3411b166904ec4b8b5efef26cd5cce52c505b3
michaelwilliams19sw <michael.williams@southworks.com>
2022-10-17 11:40:34 -0300

    primer commit

---
+++ Portfolio/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Portfolio.Models;
+using System.Diagnostics;
+
+namespace Portfolio.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            //ViewBag.Name = "Michael James Williams";
+            Person person = new Person()
+            {
+                Name = "Michael Williams",
+                Age = 24
+            };
+
+            return View("Index", person);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
